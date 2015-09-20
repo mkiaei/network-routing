2006cece396aad654484cdc61f7adf2db2b7eaae
Mohammad <mkiaei@gmail.com>
2015-09-20 00:50:16 -0400

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,24 @@
 #include "traffic.h"
 #include "solution.h"
 
-int main()
+int main(int argc, char * argv[])
 {
-	Network network("9N17L.top");
+    string top_filename, dem_filename;
+    if (argc == 3)
+    {
+        top_filename = argv[1];
+        dem_filename = argv[2];
+    }
+    else
+    {
+        top_filename = "9N17L.top";
+        dem_filename = "9N17L.dem";
+    }
+
+	Network network(top_filename.data());
 	network.print();
 
-	Traffic traffic("9N17L.dem", &network);
+	Traffic traffic(dem_filename.data(), &network);
 	traffic.print();
 
 	Solution solution(&network, &traffic);
